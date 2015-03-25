e8c473419ca4441069093c9630c0dc0b581e02ea
noloman <mlorenzo@mobiquityinc.com>
2015-03-25 10:46:19 +0100

    fixed dropbox images problem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,6 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image
-	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ },
-	:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg", 
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+:storage => :dropbox,
+:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
