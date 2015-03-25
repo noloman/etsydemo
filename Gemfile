cba6fc8703a3db39e8382bb5eebcde0dfe45e88b
noloman <mlorenzo@mobiquityinc.com>
2015-03-25 10:34:04 +0100

    trying to fix dropbox images upload

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem "paperclip", "~> 4.2"
 gem "paperclip-dropbox", ">= 1.1.7"
 gem "figaro"
+# Heroku
+gem 'rails_12factor'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, 
-	:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg", :content_type => /\Aimage\/.*\Z/,
-    :storage => :dropbox, 
+	has_attached_file :image
+	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ },
+	:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg", 
+    :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 end
