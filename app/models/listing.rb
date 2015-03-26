dd4262714968f2cefc104131f846cf84ca9db75c
noloman <manulorenzop@gmail.com>
2015-03-26 15:31:15 +0100

    added users to listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,12 +4,13 @@ class Listing < ActiveRecord::Base
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
 		:storage => :dropbox,
-		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
+		:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 		:path => ":style/:id_:filename"
 	end
-
-	validates :name, :description, :price, presence: :true
+	
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :name, :description, :image, :price, presence: :true
 	validates :price, numericality: { greater_than: 0 }
-	validates_attachment_presence :image
+
+	belongs_to :user
 end
