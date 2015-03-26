0f0552ff35ab21c4e33a16f8d063c90a384d4fa0
noloman <manulorenzop@gmail.com>
2015-03-26 14:54:28 +0100

    added validation on existing image to upload

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -11,4 +11,5 @@ class Listing < ActiveRecord::Base
 
 	validates :name, :description, :price, presence: :true
 	validates :price, numericality: { greater_than: 0 }
+	validates_attachment_presence :image
 end
